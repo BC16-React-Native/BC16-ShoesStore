01aaf1c12b4eac7941fa20f93a38a59f54ebb491
Gerard <taurusfbi04@taurusfbi04.local>
2023-02-03 16:23:21 +0700

    create Splash screen

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+import { useEffect } from 'react'
+
 import React from 'react'
 
 const App = () => {
+
+  useEffect(() => {
+    Platform.OS === 'ios'? null: SplashScreen.hide();
+  }, [])
   return (
     <View>
       <Text>App</Text>
--- android/app/src/main/java/com/shoesstore/MainActivity.java
+++ android/app/src/main/java/com/shoesstore/MainActivity.java
@@ -4,8 +4,17 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle; // here
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
